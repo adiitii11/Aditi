6986f1e39bf7a4747f22edf90132975da46ebd01
Aditi Gupta <Aditi.gupta1127@gmail.com>
2024-06-15 21:29:29 +0530

    feat:v2 of site

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,80 +1,64 @@
+import { Poppins } from "next/font/google";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import Navigation from "../../components/Navigation";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata = {
   title: "Aditi Gupta",
   description:
-    "Explore the captivating world of Aditi, a passionate sophomore studying Mechanical Engineering at NSUT. Delve into a fusion of engineering, dance, painting, and fashion, where innovation meets artistic expression. Join her journey in unraveling the mechanics of the world, all while dancing to the rhythm of creativity and painting vibrant stories on the canvas of life.",
-  themeColor: "black",
-  category: "portfolio",
-  keywords:
-    "Aditi Gupta, Student, LinkedIn, GitHub, Website, Education, B.Tech (Mechanical), Netaji Subhas University of Technology, Class XII (CBSE), Sri Venkateshwar International School, Class X (CBSE), Leadership, Activities, Resonanz, Sponsorship Department, Enactus, SM, Social Media, Project Lead, Projects, Nalum, Marketing Head, Sniffdog, Technical, C++, Algorithm Optimization, SEO, Search Engine Optimization, SMO, Social Media Optimization, Extra-curricular, Skipping, Delhi State Rope Skipping Championship, Fine Arts, Camlin Certification",
-  icons: {
-    icon: "/favicon-sqaure.png",
-    shortcut: "/favicon-sqaure.png",
-    apple: "/favicon-sqaure.png",
-  },
+    "A Mechanical Engineer at Netaji Subhas University of Technology, exploring my interest in various fields.",
+  generator: "Next.js",
+  applicationName: "Aditi Gupta",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "Aditi",
+    "NSUT",
+    "Engineer",
+    "Gupta",
+    "Netaji Subhas University of Technology",
+    "DIT",
+    "NSIT",
+  ],
+  authors: [{ name: "Aditi Gupta", url: "https://www.aditi-gupta.co.in" }],
+  creator: "Aditi Gupta",
+  publisher: "Aditi Gupta",
   openGraph: {
     title: "Aditi Gupta",
     description:
-      "Explore the captivating world of Aditi, a passionate sophomore studying Mechanical Engineering at NSUT. Delve into a fusion of engineering, dance, painting, and fashion, where innovation meets artistic expression. Join her journey in unraveling the mechanics of the world, all while dancing to the rhythm of creativity and painting vibrant stories on the canvas of life.",
-    url: "https://www.aditi-gupta.co.in/",
+      "A Mechanical Engineer at Netaji Subhas University of Technology, exploring my interest in various fields.",
+    url: "https://www.aditi-gupta.co.in",
     siteName: "Aditi Gupta",
     images: [
       {
-        url: "https://www.aditi-gupta.co.in/_next/image?url=%2Fhome-page.jpg&w=640&q=75",
-        width: 480,
-        height: 600,
-        alt: "My custom alt",
+        url: "/static/logo-square.svg",
+        width: 500,
+        height: 500,
       },
     ],
     locale: "en_US",
     type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Aditi Gupta",
+    description:
+      "A Mechanical Engineer at Netaji Subhas University of Technology, exploring my interest in various fields.",
+    images: ["https://www.aditi-gupta.co.in/static/logo-square.svg"],
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="nav-parent">
-          <nav
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              padding: 10,
-            }}
-          >
-            <p
-              style={{ color: "white", fontWeight: "bold", marginLeft: 15 }}
-              className="whole"
-            >
-              Aditi Gupta
-            </p>
-            <p
-              style={{ color: "white", fontWeight: "bold", marginLeft: 15 }}
-              className="small"
-            >
-              Aditi
-            </p>
-            <p style={{ flex: 1, textAlign: "right", marginRight: 10 }}>
-              <a className="link" href="#home">
-                Home
-              </a>
-              <a className="link" href="#about">
-                About
-              </a>
-              <a className="link" href="#resume">
-                Resume
-              </a>
-            </p>
-          </nav>
-        </div>
+      <body className={poppins.className}>
+        <Navigation></Navigation>
         {children}
-        <footer></footer>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,84 @@
-import About from "./about";
-import Art from "./art";
-import Dance from "./dance";
-import Footer from "./footer";
-import Home from "./home";
-import Line from "./line";
-import Resume from "./resume";
-import Projects from "./projects";
+import AboutBanner from "../../components/AboutBanner";
+import MainBanner from "../../components/MainBanner";
+import ProjectBanner from "../../components/ProjectBanner";
+import PortfolioBanner from "../../components/portfolio";
 
-export default function Main() {
+export default function Home() {
   return (
     <main>
-      <Home></Home>
-      <Line></Line>
-      <About></About>
-      <Line></Line>
-      <Projects></Projects>
-      <Line></Line>
-      <Dance></Dance>
-      <Line></Line>
-      <Art></Art>
-      <Line></Line>
-      <Resume></Resume>
-      <Footer></Footer>
+      <center>
+        <div style={{ position: "relative" }}>
+          <section>
+            <MainBanner />
+          </section>
+          <div
+            className="custom-shape-divider-top-1716851501"
+            style={{ margin: 0 }}
+          >
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+                className="shape-fill"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div
+          style={{
+            backgroundColor: "#ff91a4",
+            position: "relative",
+            margin: 0,
+          }}
+        >
+          <section>
+            <AboutBanner></AboutBanner>
+          </section>
+          <div
+            className="custom-shape-divider-top-1716851501"
+            style={{ margin: 0 }}
+          >
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+                className="shape-fill"
+                style={{ fill: "white" }}
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div
+          style={{
+            backgroundColor: "white",
+            position: "relative",
+            margin: 0,
+          }}
+        >
+          <section>
+            <ProjectBanner></ProjectBanner>
+          </section>
+        </div>
+        <div
+          style={{
+            backgroundColor: "#ff91a4",
+            position: "relative",
+            margin: 0,
+          }}
+        >
+          <section>
+            <PortfolioBanner></PortfolioBanner>
+          </section>
+        </div>
+      </center>
     </main>
   );
 }
